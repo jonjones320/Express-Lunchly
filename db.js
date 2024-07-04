2624ecbef7657b600c14d86484f4b3f06a911167
JJ <jonjones320@gmail.com>
2024-07-03 21:46:37 -0700

    Fixed environment variables with connecting to the Lunchly database.

--- db.js
+++ db.js
@@ -1,9 +1,15 @@
 /** Database for lunchly */
 
 const pg = require("pg");
+require('dotenv').config();
 
-const db = new pg.Client("postgresql:///lunchly");
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
 
-db.connect();
+const db = new pg.Client(`postgresql://${username}:${password}@localhost:5432/lunchly`);
+
+db.connect()
+    .then(() => console.log('Connected to the Lunchly database'))
+    .catch(err => console.error('Connection error', err));
 
 module.exports = db;
